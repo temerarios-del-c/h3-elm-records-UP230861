024c2fc2bb9efa4540f6c48e758f657cd848f562
axel alonso <up230861@alumnos.upa.edu.mx>
2026-02-15 22:51:19 -0600

    archivocompleto1

--- src/Helper.elm
+++ src/Helper.elm
@@ -1,1 +1,157 @@
 module Helper exposing (..)
+module Main exposing (..)
+
+import Html exposing (Html, div, h1, li, text, ul)
+
+
+-- =========================
+-- SIMPLE EXERCISES
+-- =========================
+
+
+add2 : Int -> Int -> Int
+add2 int1 int2 =
+    int1 + int2
+
+
+add3 : Float -> Float -> Float -> Float
+add3 int1 int2 int3 =
+    int1 + int2 + int3
+
+
+calc : Int -> Int -> (Int -> Int -> Int) -> Int
+calc int1 int2 operator =
+    operator int1 int2
+
+
+
+-- =========================
+-- RECORDS EXERCISES (1)
+-- =========================
+
+
+type alias ProgrammingLanguage =
+    { name : String
+    , releaseYear : Int
+    , currentVersion : String
+    }
+
+
+languages : List ProgrammingLanguage
+languages =
+    [ { name = "Elm", releaseYear = 2012, currentVersion = "0.19.1" }
+    , { name = "JavaScript", releaseYear = 1995, currentVersion = "ECMAScript 2025" }
+    ]
+
+
+languageNames : List ProgrammingLanguage -> List String
+languageNames langs =
+    List.map (\lang -> lang.name) langs
+
+
+
+-- =========================
+-- RECORDS EXERCISES (2)
+-- =========================
+
+
+type alias User =
+    { name : String
+    , uType : String
+    }
+
+
+users : List User
+users =
+    [ { name = "Roberto", uType = "Student" }
+    , { name = "Mitsiu", uType = "Professor" }
+    , { name = "Ana", uType = "Student" }
+    ]
+
+
+onlyStudents : List User -> List String
+onlyStudents userList =
+    List.map
+        (\user ->
+            if user.uType == "Student" then
+                user.name
+            else
+                ""
+        )
+        userList
+
+
+
+-- =========================
+-- ALIAS EXERCISE
+-- =========================
+
+
+type alias Videogame =
+    { title : String
+    , releaseYear : Int
+    , available : Bool
+    , downloads : Int
+    , genres : List String
+    }
+
+
+videogames : List Videogame
+videogames =
+    [ { title = "Control"
+      , releaseYear = 2019
+      , available = True
+      , downloads = 5000000
+      , genres = [ "Action", "Shooter" ]
+      }
+    , { title = "Ocarina of Time"
+      , releaseYear = 1998
+      , available = True
+      , downloads = 8000000
+      , genres = [ "Action", "Adventure" ]
+      }
+    ]
+
+
+getVideogameGenres : List Videogame -> List (List String)
+getVideogameGenres games =
+    List.map (\game -> game.genres) games
+
+
+
+-- =========================
+-- HTML EXERCISE
+-- =========================
+
+
+type alias Computer =
+    { ram : String
+    , model : String
+    , brand : String
+    , screenSize : String
+    }
+
+
+myLaptop : Computer
+myLaptop =
+    { ram = "16GB"
+    , model = "XPS 15"
+    , brand = "Dell"
+    , screenSize = "15.6"
+    }
+
+
+main : Html msg
+main =
+    div []
+        [ h1 [] [ text "My laptop" ]
+        , div []
+            [ ul []
+                [ li [] [ text ("Ram: " ++ myLaptop.ram) ]
+                , li [] [ text ("Modelo: " ++ myLaptop.model) ]
+                , li [] [ text ("Marca: " ++ myLaptop.brand) ]
+                , li [] [ text ("Pulgadas: " ++ myLaptop.screenSize) ]
+                ]
+            ]
+        ]
+
